b166da2a4dba786a8586d323cc476aecccee00d3
Eduardo Castor <educastor2604@gmail.com>
2024-05-11 09:58:17 -0300

    serviceRecipeV2

---
+++ APP/Models/ProductsRecipeModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace APP.Models
+{
+    public class ProductsRecipeModel
+    {
+        public long Id { get; set; }
+        public int ProductId { get; set; }
+        public virtual ProductModel? Product { get; set; }
+        public long RecipeId { get; set; }
+        public virtual RecipesModel? Recipes { get; set; }
+        public decimal? Amount { get; set; }
+        public string? Unit { get; set; }
+    }
+}
